af164fe8e64181d5127cf6e813f5d361fe28f33c
mohamed <mohamed@me>
2024-03-28 03:18:06 +0200

    16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - check tree
+ * @tree: the node
+ * Return: the node
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	printf("hello");
+
+	return (tree);
+}
